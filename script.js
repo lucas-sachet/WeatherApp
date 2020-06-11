7e18bfec7de388d549c39fea2ab48b6170b15b4f
Lucas <lucasachet@gmail.com>
2020-06-11 16:15:49 -0300

    App

---
+++ script.js
@@ -0,0 +1,46 @@
+
+// API_KEY for maps api
+let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+
+/**
+ * Retrieve weather data from openweathermap
+ */
+getWeatherData = (city) => {
+  const URL = "https://api.openweathermap.org/data/2.5/weather";
+
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=metric`;
+
+  
+  const weatherPromise = fetch(FULL_URL);
+    return weatherPromise.then((response) => {
+    return response.json();
+  })
+}
+
+/**
+ * Retrieve city input and get the weather data
+ */
+searchCity = () => {
+  const city = document.getElementById('city-input').value;
+  getWeatherData(city)
+  .then((response) => {
+    showWeatherData(response);
+  }).catch((error) => {
+    console.log(error);
+  })
+}
+
+/**
+ * Show the weather data in HTML
+ */
+showWeatherData = (weatherData) => {
+  document.getElementById('city-name').innerText = weatherData.name;
+  document.getElementById('weather-type').innerText = weatherData.weather[0].main;
+  document.getElementById('temp').innerText = weatherData.main.temp;
+  document.getElementById('feels-like').innerText = weatherData.main.feels_like;
+  document.getElementById('min-temp').innerText = weatherData.main.temp_min;
+  document.getElementById('max-temp').innerText = weatherData.main.temp_max;
+  
+}
+  
+
